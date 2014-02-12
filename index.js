c0e4e6ef9df571a5837e54c0952d441ad7487636
Luke Nimtz <luke.nimtz@gmail.com>
2014-02-12 14:12:28 -0800

    Init.

---
+++ index.js
@@ -0,0 +1,69 @@
+
+var convert = require('convert-units');
+
+module.exports = function (str,sig,strict) {
+  var sig = sig || 4;
+
+  if (!str || typeof str != 'string') return '' //throw 'metric requires a string argument'
+
+  // find all labeled measurements in string
+  var measurements = str.match(/\b\d+(\.|\/)?\d*\s?(ft|foot|feet|in|inch(es)?|lbs?|pounds?)\b/g);
+
+  if (measurements) {
+
+    for (var i = 0; i < measurements.length; i++) {
+      var org = measurements[i], orgScalar, orgUnit = org.match(/[a-z]+$/)[0], res, resScalar, resUnit, fraction;
+
+      if ( fraction = org.match(/(\d+)\/(\d+)/) ){
+        orgScalar = fraction[1] / fraction[2]
+      } else {
+        orgScalar = parseFloat(org)
+      }
+
+      switch (orgUnit){
+
+        case 'ft': case 'foot': case 'feet':
+          orgUnit = 'ft';
+          if ( orgScalar >= 3.281 ){
+            resUnit = 'm';
+          } else {
+            resUnit = 'cm';
+          }
+          break;
+
+        case 'in': case 'inch': case 'inches':
+          orgUnit = 'in';
+          if (orgScalar >= 0.3937){
+            resUnit = 'cm'; 
+          } else {
+            resUnit = 'mm';
+          }
+          break;
+
+        case 'lb': case 'lbs': case 'pound': case 'pounds':
+          orgUnit = 'lb';
+          resUnit = 'kg'; 
+          break;
+
+        default:
+          throw new Error('no conversion defined for unit: '+orgUnit) 
+      }
+
+      resScalar = convert(orgScalar).from(orgUnit).to(resUnit)
+
+      // to string with significant digit precision
+      res = resScalar.toPrecision(sig)
+
+      if (!strict){ // trim trailing zeros if sig digits is not strict
+        res = res.match(/(\d+\.\d*[1-9]+|\d+)\.?0*$/)[1]
+      }
+
+      res += ' '+resUnit;
+
+      str = str.replace(org, res)
+
+    };
+  }
+
+  return str;
+}
---
+++ test.js
@@ -0,0 +1,37 @@
+
+var assert = require('assert'),
+    metric = require('./index.js'),
+    input, expected, actual;
+
+function tc (input, expected, sig, strict) { // conversion test case
+	if (input == undefined || expected == undefined) throw new Error('invalid test args')
+	var actual = metric(input,sig,strict);
+	assert( actual == expected, '"'+input+'" -> "'+actual+'" expected: "'+expected+'"' );  
+}
+  
+console.log('testing setup');
+
+assert( metric, 'metric not defined' );
+assert( typeof metric() == 'string', 'metric did not return a string' );
+
+console.log('testing conversions');
+
+tc('2 ft','60.96 cm');
+tc('3ft','91.44 cm'); // test without space
+tc('3.281 ft','1 m'); // testing around transition
+tc('3.283 ft','1.001 m');
+tc('3.280 ft','99.97 cm');
+tc('5 feet','1.52 m',3); // full and plural names
+tc('1 inch','2.54 cm');
+tc('2 inches long','5.08 cm long');
+tc('Remote Switch, 12 ft Lead','Remote Switch, 3.66 m Lead',3); // real world examples
+tc('2 gauge, 72 in','2 gauge, 183 cm',3);
+tc('3/8 in x 80 ft','9.52 mm x 24.4 m',3);
+tc('2.5 in x 9.0 in','6.3 cm x 23 cm',2);
+tc('10.0 in x 4.5 in','25.4 cm x 11.4 cm',3,true);
+tc('21.83 in L x 6.3 in D x 6.97 in H','55.4 cm L x 16.0 cm D x 17.7 cm H',3,true);
+tc('89 lb','40.4 kg',3); // weight
+tc('12000 lb','5443 kg');
+
+
+console.log('testing passed!');
